26fdd84d8f578dea4492e6b9d964e5ddc784ebad
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-01-16 11:48:24 +0200

    Implemented basic functionalities

--- daemon.c
+++ daemon.c
@@ -4,18 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
-struct channel {
-    char* name;
-    unsigned int message_id;
-    char message[256];
-    struct channel_list_node *children;
-};
-
-struct channel_list_node {
-    struct channel *ch;
-    struct channel_list_node *next;
-};
+#include "daemon.h"
 
 struct channel * root;
 
@@ -29,7 +18,8 @@ struct channel* get_child_by_name(struct channel* parent, char* name) {
 
     // otherwise create a new channel with the given name
     struct channel * new_channel = (struct channel *) malloc(sizeof(struct channel));
-    new_channel->name = name;
+    strncpy(new_channel->name, name, 64);
+    new_channel->message_id = 0;
     memset(new_channel->message, 0, sizeof(new_channel->message));
     new_channel->children = NULL;
 
@@ -46,8 +36,8 @@ struct channel* get_child_by_name(struct channel* parent, char* name) {
         }
     }
 
-    // return the new node
-    return new_node;
+    // return the new channel
+    return new_channel;
 }
 
 // returns the channel with the given path, or creates a new channel
@@ -63,10 +53,85 @@ struct channel* get_channel(char* path) {
     return current_ch;
 }
 
+// creates a new user for the specified channel
+struct tlm * tlm_open(int type, char * channel_path) {
+    struct tlm * token = (struct tlm *)malloc(sizeof(struct tlm));
+    token->type = type;
+    token->channel = get_channel(channel_path);
+    return token;
+}
 
+// replaces the callback function with the one provided by the user
+int tlm_callback(struct tlm * token, void (* message_callback)(struct tlm * token, char * message)) {
+    token->message_callback = message_callback;
+    return 0;
+}
+
+
+const char * tlm_read(struct tlm * token, unsigned int * message_id) {
+
+    // if the user does not have read permission, then return id of -1 and empty string
+    if (token->type != TLM_SUBSCRIBER && token->type != TLM_BOTH) {
+        *message_id = -1;
+        return "\0";
+    }
+
+    // return the requested id and message
+    *message_id = token->channel->message_id;
+    return token->channel->message;
+}
+
+void replace_message(struct channel * ch, char * message) {
+
+    // replace message in channel
+    strncpy(ch->message, message, 256);
+    ch->message_id++;
+
+    // replace message in children channels
+    for(struct channel_list_node * node = ch->children; node != NULL; node = node->next) {
+        replace_message(node->ch, message);
+    }
+
+}
+
+int tlm_post(struct tlm * token, char * message) {
+
+    // no write permissions
+    if (token->type != TLM_PUBLISHER && token->type != TLM_BOTH) {
+        return -1;
+    }
+
+    // actually replace message
+    replace_message(token->channel, message);
+
+    // execute callback
+    if (token->message_callback) {
+        token->message_callback(token, token->channel->message);
+    }
+    return 0;
+}
+
+
+void tlm_close(struct tlm * token) {
+    token->message_callback = NULL;
+
+    free(token);
+    token = NULL;
+}
+
+void cleanup(struct channel * ch) {
+    while (ch->children != NULL) {
+        struct channel_list_node * temp = ch->children;
+        ch->children = ch->children->next;
+        cleanup(temp->ch);
+        free(temp);
+    }
+    ch->children = NULL;
+}
 
 
 int main() {
     root = (struct channel *) malloc(sizeof(struct channel));
+    cleanup(root);
     return 0;
 }
---
+++ daemon.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef DAEMON_H
+#define DAEMON_H
+
+#define TLM_PUBLISHER 0x1
+#define TLM_SUBSCRIBER 0x2
+#define TLM_BOTH 0x3
+
+struct channel {
+    char name[64];
+    unsigned int message_id;
+    char message[256];
+    struct channel_list_node *children;
+};
+
+struct channel_list_node {
+    struct channel *ch;
+    struct channel_list_node *next;
+};
+
+struct tlm {
+    int type;
+    struct channel * channel;
+    void (* message_callback)(struct tlm * token, char * message);
+};
+
+struct tlm * tlm_open(int type, char * channel_path);
+
+int tlm_callback(struct tlm * token, void (* message_callback)(struct tlm * token, char * message));
+
+const char * tlm_read(struct tlm * token, unsigned int * message_id);
+
+int tlm_post(struct tlm * token, char * message);
+
+void tlm_close(struct tlm * token);
+
+
+#endif
