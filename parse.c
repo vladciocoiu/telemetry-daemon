c0a5c7997059b670c2f7b9130b08683c40d0cdb3
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-01-17 15:50:30 +0200

    created parseResponse function

--- parse.c
+++ parse.c
@@ -33,6 +33,63 @@ int parseRequest(char *req, char *fd, char* op, char* ch_path, int* role, char* 
     return 0;
 }
 
+int parseResponse(char *resp, int* success, char* op, char* ch_path, int* role, char* msg) {
+    char *params[5];
+    int size = 0;
+
+    char *token = strtok(resp, " ");
+    while (token != NULL) {
+        if(size >= 5) return -1;
+
+        params[size] = token;
+        size++;
+        token = strtok(NULL, " ");
+    }
+
+    if(size < 2) return -1;
+
+    strncpy(op, params[0], 1024);
+    *success = atoi(params[1]);
+
+    // op success
+    if(strcmp(op, "deleteUser") == 0 || strcmp(op, "post") == 0) {
+        if(size != 2) return -1;
+
+        return 0;
+    }
+
+    // op success msg
+    if(strcmp(op, "read") == 0) {
+        if(size != 3) return -1;
+
+        strncpy(msg, params[2], 1024);
+
+        return 0;
+    }
+
+    // op success ch_path role
+    if(strcmp(op, "registerUser") == 0) {
+        if(size != 4) return -1;
+
+        strncpy(ch_path, params[2], 1024);
+        *role = atoi(params[3]);
+
+        return 0;
+    }
+
+    // op success ch_path role msg
+    if(strcmp(op, "callback") == 0) {
+        if(size != 5) return -1;
+
+        strncpy(ch_path, params[2], 1024);
+        *role = atoi(params[3]);
+
+        strncpy(msg, params[4], 1024);
+    }
+
+    return -1;
+}
+
 int main() {
 
     char *fd = (char*) malloc(sizeof(char) * 1024);
